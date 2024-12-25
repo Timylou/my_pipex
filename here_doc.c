3308aefef168c3848063f17e29e12886e73e1caa
Yacine El Mensi <yacine.el-mensi@etu.u-bordeaux.fr>
2024-12-25 14:04:02 +0100

    Norminette

--- here_doc.c
+++ here_doc.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   here_doc.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yel-mens <yel-mens@student.42.fr>          +#+  +:+       +#+        */
+/*   By: yel-mens <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/24 17:23:25 by yel-mens          #+#    #+#             */
-/*   Updated: 2024/12/24 19:15:37 by yel-mens         ###   ########.fr       */
+/*   Updated: 2024/12/25 14:03:35 by yel-mens         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,37 +40,41 @@ static char	*ft_read_line(int fd)
 	return (line);
 }
 
+static void	ft_read_stdin(int end[2], char *limiter, t_cmd *cmd, char **path)
+{
+	char	*line;
+
+	close(end[0]);
+	write(1, "here_doc> ", 10);
+	line = ft_read_line(STDIN_FILENO);
+	while (line)
+	{
+		if (!ft_strncmp(line, limiter, ft_strlen(limiter)))
+		{
+			free(line);
+			ft_free_struct(cmd, path);
+			exit(EXIT_SUCCESS);
+		}
+		write(end[1], line, ft_strlen(line));
+		write(1, "here_doc> ", 10);
+		free(line);
+		line = ft_read_line(STDIN_FILENO);
+	}
+	ft_free_struct(cmd, path);
+	exit(EXIT_FAILURE);
+}
+
 int	here_doc(char *limiter, t_cmd *cmd, char **path)
 {
 	pid_t	pid;
 	int		end[2];
 	int		new_end[2];
-	char	*line;
 
 	if (pipe(end) < 0)
 		return (0);
 	pid = fork();
 	if (!pid)
-	{
-		close(end[0]);
-		write(1, "here_doc> ", 10);
-		line = ft_read_line(STDIN_FILENO);
-		while (line)
-		{
-			if (!ft_strncmp(line, limiter, ft_strlen(limiter)))
-			{
-				free(line);
-				ft_free_struct(cmd, path);
-				exit(EXIT_SUCCESS);
-			}
-			write(end[1], line, ft_strlen(line));
-			write(1, "here_doc> ", 10);
-			free(line);
-			line = ft_read_line(STDIN_FILENO);
-		}
-		ft_free_struct(cmd, path);
-		exit(EXIT_FAILURE);
-	}
+		ft_read_stdin(end, limiter, cmd, path);
 	close(end[1]);
 	waitpid(pid, NULL, 0);
 	if (pipe(new_end) < 0)
